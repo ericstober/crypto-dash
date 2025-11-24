42f6b5245fde36408786b60f8ce9becc1c3294c0
ericstober <erictstober@gmail.com>
2025-11-24 15:44:57 -0600

    add CoinCard component to display crypto coin information

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import CoinCard from "./components/CoinCard";
 const API_URL =
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
 
@@ -28,6 +29,16 @@ const App = () => {
   return (
     <div>
       <h1>🚀 Crypto Dash</h1>
+      {loading && <p>Loading...</p>}
+      {error && <div class='error'>{error}</div>}
+
+      {!loading && !error && (
+        <main className='grid'>
+          {coins.map((coin) => (
+            <CoinCard key={coin.id} coin={coin} />
+          ))}
+        </main>
+      )}
     </div>
   );
 };
