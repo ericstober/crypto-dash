3d5f618b29731294f0750a6ecdf93e6c9f0fb47c
ericstober <erictstober@gmail.com>
2025-11-24 14:52:56 -0600

    modify api fetch to use async await

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,21 @@ const App = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(API_URL)
-      .then((res) => {
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(API_URL);
         if (!res.ok) throw new Error("Failed to fetch data");
-        return res.json();
-      })
-      .then((data) => {
+        const data = await res.json();
         console.log(data);
         setCoins(data);
-        setLoading(false);
-      })
-      .catch((error) => {
+      } catch (error) {
         setError(error.message);
+      } finally {
         setLoading(false);
-      });
+      }
+    };
+
+    fetchCoins();
   }, []);
 
   return (
